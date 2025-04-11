ba94fa7bf0b973118f0097592e1f43e12f90546b
won6c <superken1227@naver.com>
2025-04-11 21:25:03 +0900

    [과제5]클래스 매서드와 함수 리턴 타입 이용한 간단한 책 대여 서비스 만들기

--- shell_script2.py
+++ shell_script2.py
@@ -2,25 +2,25 @@ from books.models import Book
 
 #1. 전체 책 조회
 all_books = Book.get_all_books()
-print("전체 책 목록:")
+print("📚전체 책 목록:")
 for book in all_books:
     print(book)
 
 #2. 특정 저자 책만 조회
 orwell_books = Book.get_books_by_author("George Orwell")
-print("\nGeorge Orwell의 책 목록:")
+print("\n🖋️ George Orwell의 책 목록:")
 for book in orwell_books:
     print(book)
 
 #3. 제목 키워드로 검색
 dystopia_books = Book.get_books_by_title_keyword("new")
-print("\n제목에 'new'가 포함된 책 목록:")
+print("\n🔍제목에 'new'가 포함된 책 목록:")
 for book in dystopia_books:
     print(book)
 
 #4. 제목순 정렬
 sorted_books = Book.get_books_ordered_by_title()
-print("\n제목순 정렬:")
+print("\n🔠제목순 정렬:")
 for book in sorted_books:
     print(book)
 
